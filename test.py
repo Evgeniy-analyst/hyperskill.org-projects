50c8702dda3924d2e64f80025efb2dd6d825605e
[Evgeniy] <[tron1100@mail.ru]>
2021-07-27 12:39:32 +0300

    fix

---
+++ test.py
@@ -0,0 +1,9 @@
+feedback = 'В подсолнухах мне больше всего понравился заботливый персонал'
+end = len(feedback)
+print(end)
+start = end - len('заботливый персонал')
+print(len('заботливый персонал'))
+print(start)
+service = feedback[start:end]
+print(feedback[start:end])
+print(service)
